7a38f147d811b48fbcaee35d517dee6a47855654
akhilSNediyathu <akhilsnediyathu@gmail.com>
2024-03-11 22:56:12 +0530

    week2day3

---
+++ week 2/insertion_sort.dart
@@ -0,0 +1,18 @@
+void insertionSort(arr){
+  int a = arr.length;
+  for (var i = 1; i <a ; i++) {
+    int temp = arr[i];
+    int j = i-1;
+    while (j>=0&& arr[j]>temp) {
+      arr[j+1]=arr[j];
+      
+      j--;
+          }
+    arr[j+1]=temp;
+  }
+print(arr);
+}
+void main(){
+  List arr = [9,2,8,6,4,5,7,6];
+  insertionSort(arr); 
+}
